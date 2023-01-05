21309d2f19fa63476eb1d19ab2a421c5f1f54860
Leonard Blam <blam@g.jct.ac.il>
2023-01-06 01:17:24 +0200

    finally fixed, index works

--- script2.js
+++ script2.js
@@ -14,6 +14,28 @@ let lrise = 0;
 let day12slider = 0;
 // let sunrise;
 // let sunset;
+let nighthour = 0;
+let nightmin = 0;
+let nighthoursplit = 0;
+let nightminsplit = 0;
+let totalhour = 0;
+let dhour = 0;
+let dmin = 0;
+let ehour = 0;
+let emin = 0;
+let fhour = 0;
+let fmin = 0;
+let tot = 0;
+let xhour = 0;
+let xmin = 0;
+let itmin = 0;
+let currentperiodhour = 0;
+let currentperiodmin = 0;
+let timearrayh = [];
+let timearraym = [];
+let nightarrayh = [];
+let nightarraym = [];
+let currentperiod = 0;
 
 
 async function getSunriseSunset(latitude, longitude) {
@@ -46,8 +68,8 @@ dayLenght();
 setInterval(dayLenght, 1000);
 
 const sunrise = () => {
-getSunriseSunset(47.559501, 7.588576).then((data) => {
-    const sunr = data.results.sunrise;
+getSunriseSunset(47.559601, 7.588576).then((data) => {
+    const sunr = data.results.civil_twilight_begin;
     let slicerise;
     if (sunrise[4] != ":") {
     slicerise = sunr.slice(0, 4);
@@ -62,8 +84,8 @@ getSunriseSunset(47.559501, 7.588576).then((data) => {
 }
 
 const sunset = () => {
-    getSunriseSunset(47.559501, 7.588576).then((data) => {
-    const suns = data.results.sunset;
+    getSunriseSunset(47.559601, 7.588576).then((data) => {
+    const suns = data.results.civil_twilight_end;
     let sliceset;
     if (sunset[4] != ":") {
     sliceset = suns.slice(0, 4);
@@ -73,7 +95,7 @@ const sunset = () => {
     }
 
     temp = sliceset.split(":");
-    temp[0] = -(temp[0] - 18);
+    temp[0] = parseInt(temp[0]) + 12;
     sliceset = temp.join(":");
 
     // return sliceset; // Set the sunset variable to the sliceset value
@@ -108,6 +130,69 @@ const difference = () => {
 difference();
 setInterval(difference, 10000);
 
+const diff = () => {
+    
+
+if (lset != 0 || lrise != 0) {
+    dhour = lset.slice(0, 2);
+    dmin = lset.slice(3, 5);
+    ehour = lrise.slice(0, 2);
+    emin = lrise.slice(2, 4);
+    fhour = parseInt(dhour) - parseInt(ehour);
+    fmin = parseInt(dmin) - parseInt(emin);
+    // tot = fhour + ":" + fmin;
+    
+    itmin = parseInt((fhour * 60) / 12 + fmin / 12);
+    while (itmin > 60) {
+    xhour += 1;
+    itmin = itmin - 60;
+    }
+    if (itmin < 10) {
+    itmin = "0" + itmin;
+    }
+    tot = xhour + ":" + itmin;
+    nighthour = 24 - fhour;
+    nightmin = 60 - fmin;
+    if (nightmin < 0) {
+    nightmin = 60 + nightmin;
+    nighthour -= 1;
+    
+    }
+    totalhour = fhour + ":" + fmin;
+    }
+    
+    nighthoursplit = parseInt(nighthour / 4);
+    nightminsplit = parseInt(nightmin / 4);
+    // console.log("night: " + nighthoursplit + ":" + nightminsplit);
+
+    // console.log(emin);
+    for (let i = 0; i < 12; i++) {
+        timearrayh[i] = parseInt(ehour) + i * xhour;
+        timearraym[i] = parseInt(emin) + i * itmin;
+        // console.log("time: " + timearraym[0]);
+        while (timearraym[i] > 60) {
+            timearraym[i] = timearraym[i] - 60;
+            timearrayh[i] += 1;
+        }
+    }
+
+    for (let i = 0; i < 4; i++) {
+        nightarrayh[i] = parseInt(dhour) + i * nighthoursplit;
+        nightarraym[i] = parseInt(dmin) + i * nightminsplit;
+        while (nightarraym[i] > 60) {
+            nightarraym[i] = nightarraym[i] - 60;
+            nightarrayh[i] += 1;
+        }
+        while (nightarrayh[i] > 24) {
+            nightarrayh[i] = nightarrayh[i] - 24;
+        }
+        // console.log("night: " + nightarrayh[i] + ":" + nightarraym[i]);
+    }
+}    
+
+diff();
+setInterval(diff, 10000);
+
 const setClock = () => {
     let day = new Date();
     let hh = day.getHours();
@@ -124,12 +209,52 @@ const setClock = () => {
     }
 // get time now
 
-    let nighthour = 24 - Lhour;
-    let nightmin = 60 - Lmin;
-    let nightsec = 60 - Lsec;
+let dayornight = "day";
+currentperiodhour = parseInt(hh) + xhour;
+currentperiodmin = parseInt(mm) + itmin;
+if (currentperiodmin > 60) {
+    currentperiodmin = currentperiodmin - 60;
+    currentperiodhour += 1;
+    }
+if (dayornight == "day"){
+    for (let j = 0; j < 12; j++) {
+        // console.log(timearrayh[j] + ":" + timearraym[j]);
+        if (hh >= timearrayh[j] && hh < dhour) {
+            currentperiod = j +1;
+            dayornight = "day";
+        }
+        else if (hh >= dhour){
+            dayornight = "night";
+        }
+    }
+}
+if (dayornight == "night") {
+    for (let j = 0; j < 4; j++) {
+            // console.log(timearrayh[j] + ":" + timearraym[j]);
+            if (hh >= nightarrayh[j] && hh <= 24 && hh > 12 && nightarrayh[j] > 12 || hh >= nightarrayh[j] && hh >= 0 && hh < ehour) {
+                currentperiod = j +1;
+                dayornight = "night";
+            }
+            else if (hh >= ehour){
+                dayornight = "day";
+            }
+    }
+}
+th = "th";
+if (currentperiod == 1) {
+    th = "st";
+}
+else if (currentperiod == 2) {
+    th = "nd";
+}
+else if (currentperiod == 3) {
+    th = "rd";
+}
 
+// let night = nighthour + ":" + nightmin;
+// console.log(nightarrayh[3] + ":" + nightarraym[3]);
 document.getElementById("d-clock").innerHTML = hh + ":" + mm + ":" + ss;
-document.getElementById("sunclock").innerHTML = "It is the " + day12slider + " of the day";
+document.getElementById("sunclock").innerHTML = "It is the " + currentperiod + " " + th + " hour of the " + dayornight;
 document.getElementById("suntime").innerHTML = "Sunrise: " + lrise + " Sunset: " + lset;
 };
 
