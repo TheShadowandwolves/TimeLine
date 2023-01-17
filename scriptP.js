3789a0c315a2b70c49e0ca035dd13bb3cda44482
Leonard Blam <blam@g.jct.ac.il>
2023-01-17 17:24:33 +0200

    notification

--- scriptP.js
+++ scriptP.js
@@ -11,37 +11,28 @@ function switchDiv(id) {
   }
 switchDiv("div1");
 
-// function checkViewport() {
-//     const elementct = document.getElementById("ct");
-//     const subelement = document.getElementById("subct");
-//     const min_margin_sub = 0;
-//     const minSize = 0.2;
-//     const minSizesub = 0.2;
-//     const maxSize = 2;
-//     const maxSizesub = 1.5;
-//     const viewportWidth = window.innerWidth;
-//     let fontSize = 0;
-//     let fontSizesub = 0;
- 
-//     if (viewportWidth < 600) {
-//         fontSize = minSize;
-//         fontSizesub = minSizesub;
-//         subelement.style.paddingLeft = min_margin_sub + "px";
-//     } else if (viewportWidth > 1200) {
-//         fontSize = maxSize;
-//         fontSizesub = maxSizesub;
-//     } else {
-//         fontSize = (viewportWidth - 600) / 600 * (maxSize - minSize) + minSize;
-//         fontSizesub = (viewportWidth - 600) / 600 * (maxSizesub - minSizesub) + minSizesub;
-//     }
- 
-//     elementct.style.fontSize = fontSize + "rem";
-//     subelement.style.fontSize = fontSizesub + "rem";
-//  }
- 
-//  // Call the function when the page loads
-//  window.addEventListener("load", checkViewport);
- 
-//  // Call the function when the window is resized
-//  window.addEventListener("resize", checkViewport);
- 
+function showWarning(){
+  // Check if the screen width is less than 600 pixels
+  if (window.innerWidth < 600) {
+    // Get the message element by its id
+    const messageElement = document.getElementById("text-message");
+    // Show the message
+    messageElement.style.display = "block";
+    // Write the message
+    messageElement.innerHTML = "This site is not optimized for mobile devices. Please use a desktop or laptop computer for the best experience.";
+  }
+  else {
+    // Get the message element by its id
+    const messageElement = document.getElementById("message");
+    // Hide the message
+    messageElement.style.display = "none";
+  }
+}
+// Call the function when the page loads
+window.addEventListener("load", showWarning);
+
+function hide(){
+  const messageElement = document.getElementById("message");
+  messageElement.style.display = "none";
+}
+
